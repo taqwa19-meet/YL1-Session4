54d5d0e96a76aea8e4a03cf2e87607f137d9498e
idanz19-meet <idanz19@meet.mit.edu>
2017-11-08 19:16:10 +0200

    lab4 code

---
+++ Lab4.py
@@ -0,0 +1,22 @@
+
+
+#class Animal(object):
+    #def __init__(self,sound,name,age,favorite_color):
+        #self.sound= sound
+        #self.name= name
+        #self.age= age
+        #self.favorite_color=favorite_color
+    #def eat(self,food):
+        #print("yummy!!"+self.name+"is eating"+food)
+    #def description(self):
+       # print(self.name+"is"+str(self.age)+"years old and loves the color"+self.favorite_color)
+    #def make_sound(self):
+        #print(self.sound *3)
+              
+#jeffchimp = Animal("meow","jeffchimp",13,"rainbow")
+#jeffchimp.description()
+
+#jeffchimp.make_sound()
+
+class Person (object):
+    def__init__
